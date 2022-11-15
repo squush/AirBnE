ee555d20e7f2553ab77589b85081f9ce7d196a95
Andrew Wever <andrew.wever@gmail.com>
2022-11-15 15:21:05 -0500

    Set routes for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    # No new route for bookings. We'll just have the form directly on the crime
+    # page so the user can book without going to a new page
+    resources :bookings, only: %i[index show create]
+  end
   root to: "pages#home"
 
   # index show new create edit update destroy
   resources :crimes do
-
   end
+
+  resources :bookings, only: %i[destroy]
 end
