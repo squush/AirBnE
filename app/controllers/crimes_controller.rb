0e1363d9f6b18afca9f1e0d9b9709cc5c4e5f830
Andrew Wever <andrew.wever@gmail.com>
2022-11-15 12:27:24 -0500

    Can view a single crime. Also added a test user in the seed, and improved the seed

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -1,9 +1,13 @@
 class CrimesController < ApplicationController
+  # Jackson
   def index
     @crimes = Crime.all
   end
 
+  # Jackson made the initial
+  # Andrew made the rest
   def show
     @crime = Crime.find(params[:id])
+    @user = User.find(@crime.user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Jackson
   get '/crimes', to: 'crimes#index'
+  # Jackson
   get '/crimes/:id', to: 'crimes#show'
 end
