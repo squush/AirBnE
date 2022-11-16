a93406073dbc0b72bf3dc7c42b3b3dd98221550d
Jackson Smylie <jacksonsmylie@gmail.com>
2022-11-15 22:34:11 -0500

    added a few comments for more clarity

--- app/javascript/controllers/filter_type_controller.js
+++ app/javascript/controllers/filter_type_controller.js
@@ -7,13 +7,16 @@ export default class extends Controller {
   }
 
   toggle() {
+    // Toggles class each click
     this.element.classList.toggle("clicked");
-    // Jackson - started to implement filtering logic, TODO
+
+    // Get all crime cards and all crime type filters selected
     const types_clicked = Array.from(document.getElementsByClassName("clicked"));
     const crime_cards = Array.from(document.getElementsByClassName("crime-card"));
     let types_to_show = [];
     types_clicked.forEach(type => types_to_show.push(type.innerText));
 
+    // Default to displaying all crimes, hide all crimes that were not selected
     if (types_to_show.length === 0) {
       crime_cards.forEach(card => { card.style.display = "block" });
     } else {
