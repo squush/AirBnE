0f4035335e380705a86dffcbe82b7f0bad73bded
Andrew Wever <andrew.wever@gmail.com>
2022-11-16 16:36:41 -0500

    add a TODO to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  # index show new create edit update destroy
   resources :crimes do
+    # TODO: Remove the new here. Just make the booking via a form directly on
+    #       the crime's show page. This will require changes downstream.
     resources :bookings, only: %i[new create]
   end
 
@@ -13,8 +14,5 @@ Rails.application.routes.draw do
   get '/approve/:id', to: "bookings#approve", as: :approve_booking
   get '/reject/:id', to: "bookings#reject", as: :reject_booking
 
-  # No new route for bookings. We'll just have the form directly on the crime
-  # page so the user can book without going to a new page
-  # resources :bookings, only: %i[]
   resources :bookings, only: %i[index show destroy]
 end
