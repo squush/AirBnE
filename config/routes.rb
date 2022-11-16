a42ce66de33d3a7a1e6b7e2cd1a6586beede3b64
Andrew Wever <andrew.wever@gmail.com>
2022-11-16 16:40:06 -0500

    crime edit and update not needed now

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :crimes do
+  resources :crimes, only: %i[index new create show delete] do
     # TODO: Remove the new here. Just make the booking via a form directly on
     #       the crime's show page. This will require changes downstream.
     resources :bookings, only: %i[new create]
