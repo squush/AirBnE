09c695e88722be5255ac532de3e1084f6e5edb5b
Andrew Wever <andrew.wever@gmail.com>
2022-11-17 11:45:36 -0500

    Geocoder basics added, but nothing visible to user yet

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -5,4 +5,7 @@ class Crime < ApplicationRecord
   validates :crime_type, presence: true
   validates :area, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  geocoded_by :area
+  after_validation :geocode, if: :will_save_change_to_area?
 end
